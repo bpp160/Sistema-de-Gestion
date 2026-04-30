1fa87eba6d7d84e770911f62dbc7ceba25c7d132
Emanuel890178 <emanuelu258@gmail.com>
2026-04-29 21:45:10 -0400

    Add files via upload

--- bd_notificacion.sql
+++ bd_notificacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-04-2026 a las 15:48:23
+-- Tiempo de generación: 30-04-2026 a las 03:36:03
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,7 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `notificacion` (
-  `ID_NOTIFI` int(11) NOT NULL
+  `id_notificaion` bigint(20) NOT NULL,
+  `usuario_id` bigint(20) NOT NULL,
+  `mensaje` text NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
+  `leido` tinyint(1) DEFAULT 0,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -39,7 +44,17 @@ CREATE TABLE `notificacion` (
 -- Indices de la tabla `notificacion`
 --
 ALTER TABLE `notificacion`
-  ADD PRIMARY KEY (`ID_NOTIFI`);
+  ADD PRIMARY KEY (`id_notificaion`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `notificacion`
+--
+ALTER TABLE `notificacion`
+  MODIFY `id_notificaion` bigint(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
